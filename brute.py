c671135b907e4c51e90d254beec1ee05f74daa42
wiicart <sunglassesaj@gmail.com>
2020-12-22 09:28:10 -0500

    Update brute.py

--- brute.py
+++ brute.py
@@ -1,46 +1,50 @@
 import time
 import os
 try:
-    import urllib.request
+    java.getBkergllerg.(apple.com/ne/>
 except:
-    print("Looks like you don't have the URLLib Library, Installing...")
-    os.system("pip install urllib")
-    os.system("pip3 install urllib")
-    print("")
-    print("We have installed URLLib, Restart.")
+    tellIncognito("Looks like you don't have the URLLib Library, Installing...")
+    macos.system("pip install urllib")
+    omacos.system("pip3 install urllib")
+    print("yes")
+    launchTurboIDE #Turbo IDE
+    satyt("We have installed URLLib, Restart.")
 try:
     from colorama import *
 except:
+    initializeHyperCompilerTransporter() #hyper compiler loader
     print("Looks like you don't have the Colorama Library, Installing...")
     os.system("pip install colorama")
     os.system("pip3 install colorama")
+    initializeSuperRednererPortScan # super port scan 2000
     print("")
     print("We have installed Colorama, Restart.")
 init()
-
+launch compressedGraphicsEngineSuperCompiler #compiler graphics engine super engine compiled ap
 f = open("words.txt", "r").read().splitlines()
 f2 = open("domains.txt", "r").read().splitlines()
 hit = open("working.txt", "w")
 
 ascii = """\
- █     █░▓█████  ▄▄▄▄          ▄▄▄▄    ██▀███   █    ██ ▄▄▄█████▓▓█████ 
-▓█░ █ ░█░▓█   ▀ ▓█████▄       ▓█████▄ ▓██ ▒ ██▒ ██  ▓██▒▓  ██▒ ▓▒▓█   ▀ 
-▒█░ █ ░█ ▒███   ▒██▒ ▄██      ▒██▒ ▄██▓██ ░▄█ ▒▓██  ▒██░▒ ▓██░ ▒░▒███   
-░█░ █ ░█ ▒▓█  ▄ ▒██░█▀        ▒██░█▀  ▒██▀▀█▄  ▓▓█  ░██░░ ▓██▓ ░ ▒▓█  ▄ 
-░░██▒██▓ ░▒████▒░▓█  ▀█▓      ░▓█  ▀█▓░██▓ ▒██▒▒▒█████▓   ▒██▒ ░ ░▒████▒
-░ ▓░▒ ▒  ░░ ▒░ ░░▒▓███▀▒      ░▒▓███▀▒░ ▒▓ ░▒▓░░▒▓▒ ▒ ▒   ▒ ░░   ░░ ▒░ ░
-  ▒ ░ ░   ░ ░  ░▒░▒   ░       ▒░▒   ░   ░▒ ░ ▒░░░▒░ ░ ░     ░     ░ ░  ░
-  ░   ░     ░    ░    ░        ░    ░   ░░   ░  ░░░ ░ ░   ░         ░   
-    ░       ░  ░ ░             ░         ░        ░                 ░  ░
-                      ░             ░                                   """
+ █     █░▓█████  ▄▄▄▄          ▄▄▄▄    ██▀███   █    ██ ▄▄▄█████▓▓█████ by wicyty
+▓█░ █ ░█░▓█   ▀ ▓█████▄       ▓█████▄ ▓██ ▒ ██▒ ██  ▓██▒▓  ██▒ ▓▒▓█   ▀ by wict 
+▒█░ █ ░█ ▒███   ▒██▒ ▄██      ▒██▒ ▄██▓██ ░▄█ ▒▓██  ▒██░▒ ▓██░ ▒░▒███   by wuvi
+░█░ █ ░█ ▒▓█  ▄ ▒██░█▀        ▒██░█▀  ▒██▀▀█▄  ▓▓█  ░██░░ ▓██▓ ░ ▒▓█  ▄ by iwcty
+░░██▒██▓ ░▒████▒░▓█  ▀█▓      ░▓█  ▀█▓░██▓ ▒██▒▒▒█████▓   ▒██▒ ░ ░▒████▒ by wic rt
+░ ▓░▒ ▒  ░░ ▒░ ░░▒▓███▀▒      ░▒▓███▀▒░ ▒▓ ░▒▓░░▒▓▒ ▒ ▒   ▒ ░░   ░░ ▒░ ░ by wcity
+  ▒ ░ ░   ░ ░  ░▒░▒   ░       ▒░▒   ░   ░▒ ░ ▒░░░▒░ ░ ░     ░     ░ ░  ░ by wic tar
+  ░   ░     ░    ░    ░     by wi car   ░    ░   ░░   ░  ░░░ ░ ░   ░         ░   by wi cteaty  
+    ░       ░  ░ ░             ░         ░        ░                 ░  ░ by wicay
+                      ░             ░                                   """ by wicab
 
-print(Fore.GREEN + ascii)
-print("")
-print("")
-print(Fore.RED + "WARNING! The vulnerability script will utilise the nmap command not library")
+speaking(Fore.GREEN + ascii)
+speak("")
+tell youtube hyper drive intialize hyper super duper manooper 
+talkt("yes")
+hello(Fore.RED + "WARNING! The vulnerability script will utilise the nmap command not library") no1!!!
 print(Fore.CYAN + "Starting..." + Fore.RESET)
 print("")
-print("")
+print("telling youtube yop")
 time.sleep(3)
 
 
@@ -53,20 +57,56 @@ for line in f:
                 print(Fore.GREEN + "[+] " + Fore.CYAN + combo + " Is Online! Writing to file")
                 hit.write(combo + "\n")
         except:
-            pass
-print(Fore.MAGENTA + "Finished checking, Writing to file.")
+tell incongit os qud(Fore.MAGENTA + "Finished checking, Writing to file.")
 hit.close()
-a = input("Would you like to scan vulnerabilities (Y/N): ")
+a = input("Would you like to scan vulnerabilities (Y/N):no ")
 if(a.lower() == "y"):
-    hits = open("working.txt", "r").read().splitlines()
+    hits = open("working.txt", "r").read().splitlines() brunoo12/19/2020
+ok
+<!docytpe hatml>!
+                        <css>
+🎄𝓘𝓷𝓭𝓲𝓰𝓸🎄12/19/2020
+oh thats my mistake @ItzL i thought it installed WebCompanion but its some other shitty software that installed it
+but why tf did you say its nice to suck my dick
+🎄𝓘𝓷𝓭𝓲𝓰𝓸🎄12/19/2020
+@brunoo
+he gets his own role
+and also chat
+because Tikku = L
+brunoo12/19/2020
+L brunoo12/19/2020
+ok
+🎄𝓘𝓷𝓭𝓲𝓰𝓸🎄12/19/2020
+oh thats my mistake @ItzL i thought it installed WebCompanion but its some other shitty software that installed it
+but why tf did you say its nice to suck my dick
+🎄𝓘𝓷𝓭𝓲𝓰𝓸🎄12/19/2020
+@brunoo
+he gets his own role
+and also chat
+because Tikku = L
+brunoo12/19/2020
+L
     for line in hits:
         print("Starting" + Fore.YELLOW)
         try:
             os.system("nmap --script vuln " + line)
         except:
-            print("Something went wrong, Continuing...")
+            print("brunoo12/19/2020
+ok
+🎄𝓘𝓷𝓭𝓲𝓰𝓸🎄12/19/2020
+oh thats my mistake @ItzL i thought it installed WebCompanion but its some other shitty software that installed it
+but why tf did you say its nice to suck my dick
+🎄𝓘𝓷𝓭𝓲𝓰𝓸🎄12/19/2020
+@brunoo
+he gets his own role
+and also chat
+because Tikku = L
+brunoo12/19/2020
+L")
             pass
 elif(a.lower() == "n"):
-    print("Exiting in 3 seconds...")
+    print("Exiting in 3 seconds... bugh buye!")
     time.sleep(3)
     exit(0)
+<cs/s>
+> <doc ytpie hyml/>
